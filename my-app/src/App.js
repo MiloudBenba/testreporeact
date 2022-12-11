98cd3436182a33490bd5fb3b634fe50776d9a3f3
Mimotest59 <myloud59@hotmail.fr>
2022-12-11 14:27:31 +0100

    docs: created first css and navbar component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./conponents/Navbar";
-import Test from "./conponents/Test";
 
 function App() {
   return (
     <>
       <div className="App">
-        <Navbar />
         <header className="App-header">
-          <Test />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     </>
